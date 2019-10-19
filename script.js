e8d1d5404de3cc3a9d0f1a616d96189c6bdb4f0a
Daniel <caudaDelphini@gmail.com>
2019-10-19 14:41:23 -0400

    Remove book.js

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
-import {Book} from "./book";
+class Book {
+    constructor(title, author, pages, read) {
+        this._title = title;
+        this._author = author;
+        this._pages = pages;
+        this._read = read;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(value) {
+        this._title = value;
+    }
+
+    get author() {
+        return this._author;
+    }
+
+    set author(value) {
+        this._author = value;
+    }
+
+    get pages() {
+        return this._pages;
+    }
+
+    set pages(value) {
+        this._pages = value;
+    }
+
+    get read() {
+        return this._read;
+    }
+
+    set read(value) {
+        this._read = value;
+    }
+}
 
 let library = JSON.parse(localStorage.getItem("library")) || [];
 
