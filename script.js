124b3b4d58c21958ac8ff31cdb0a694a22ed99b9
Daniel <caudaDelphini@gmail.com>
2019-10-19 14:04:35 -0400

    Implement library

---
+++ script.js
@@ -0,0 +1,100 @@
+import {Book} from "./book";
+
+let library = JSON.parse(localStorage.getItem("library")) || [];
+
+function updateLibrary() {
+    localStorage.setItem("library", JSON.stringify(library));
+}
+
+function makeBook(i, shelfDiv) {
+    let book = library[i];
+    let bookDiv = document.createElement("div");
+    bookDiv.classList.add("book-card");
+    bookDiv.addEventListener("click", () => {
+        book.read = !book.read;
+        if (book.read) {
+            bookDiv.classList.add("read-book");
+        } else {
+            bookDiv.classList.remove("read-book");
+        }
+        updateLibrary();
+    });
+    if (book.read) {
+        bookDiv.classList.add("read-book");
+    }
+
+    let titleDiv = document.createElement("div");
+    titleDiv.textContent = book.title || "Default";
+    titleDiv.classList.add("book-title");
+
+    let horizontalRule = document.createElement("hr");
+    horizontalRule.classList.add("horizontal-rule");
+    let authorDiv = document.createElement("div");
+    authorDiv.textContent = book.author || "unknown";
+    let pagesDiv = document.createElement("div");
+    pagesDiv.textContent = book.pages || 0;
+    let removeButton = document.createElement("button");
+    removeButton.textContent = "remove";
+    removeButton.classList.add("book-button", "pink-background");
+    removeButton.addEventListener("click", () => {
+        library.splice(i, 1);
+        shelfDiv.removeChild(bookDiv);
+        updateLibrary();
+    });
+
+    bookDiv.append(titleDiv, horizontalRule, authorDiv, pagesDiv, removeButton);
+    return bookDiv;
+}
+
+function render() {
+    let shelfDiv = document.querySelector("#shelf");
+    for (let i = 0; i < library.length; i++) {
+        shelfDiv.appendChild(makeBook(i, shelfDiv));
+    }
+}
+
+let newBookButton = document.querySelector("#new-book-button");
+newBookButton.addEventListener("click", () => {
+    let shelfDiv = document.querySelector("#shelf");
+
+    let newBookDiv = document.createElement("div");
+    newBookDiv.classList.add("book-card");
+
+    let newBookTitle = document.createElement("input");
+    newBookTitle.type = "text";
+    newBookTitle.placeholder = "title";
+    newBookTitle.classList.add("field");
+    let newBookAuthor = document.createElement("input");
+    newBookAuthor.type = "text";
+    newBookAuthor.placeholder = "author";
+    newBookAuthor.classList.add("field");
+    let newBookPages = document.createElement("input");
+    newBookPages.type = "number";
+    newBookPages.placeholder = "pages";
+    newBookPages.classList.add("field");
+
+    let cancelButton = document.createElement("button");
+    cancelButton.textContent = "cancel";
+    cancelButton.classList.add("book-button", "pink-background");
+    cancelButton.addEventListener("click", () => {
+        shelfDiv.removeChild(newBookDiv);
+    });
+
+    let submitButton = document.createElement("button");
+    submitButton.textContent = "submit";
+    submitButton.classList.add("book-button", "cyan-background");
+    submitButton.addEventListener("click", () => {
+        library.push(new Book(newBookTitle.value,
+            newBookAuthor.value,
+            newBookPages.valueAsNumber));
+        shelfDiv.appendChild(makeBook(library.length - 1, shelfDiv));
+        shelfDiv.removeChild(newBookDiv);
+        updateLibrary();
+    });
+
+    newBookDiv.append(newBookTitle, newBookAuthor, newBookPages, cancelButton, submitButton);
+    shelfDiv.appendChild(newBookDiv);
+    newBookDiv.scrollIntoView(true);
+});
+
+render();
